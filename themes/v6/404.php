8f10bd72f3120426f4fb3fd14468fb56e6c03f32
Tim Smith <tim@timothybsmith.com>
2013-03-22 14:33:25 -0500

    Made 404 page work

--- themes/v6/404.php
+++ themes/v6/404.php
@@ -1,10 +1,13 @@
 <?php @header("HTTP/1.1 404 Not found", TRUE, 404); ?>
 <?php get_header() ?>
 
-<div class="main-content">
-  <div class="entry">
-        <h2>Awwwww snap! What happened? This page doesn't exist!</h2>
-        <p>Should something be here? <a href="/contact">Get in touch</a></p>
-  </div><!-- end .entry -->
-</div><!-- end .main-content -->
+<section role="main" class="main-content">
+  <div class="contain">
+    <div class="content">
+        <h1 class="statement">Awwwww snap! What happened? This page doesn't exist!</h2>
+        <p>Should something be here? <a href="/contact">Get in touch</a>. Hey look! Cat mittens!</p>
+        <img src="<?php bloginfo('template_url');?>/img/cat-mittens.gif" alt="Cat Mittens Gif">
+    </div><!-- end .content -->
+  </div><!-- end .contain -->
+</section><!-- end .main-content -->
 <?php get_footer(); ?>
