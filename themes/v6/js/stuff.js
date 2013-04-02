f9b982f3fa008ccb32ab5e37dd22a2218a013c42
Tim Smith <tim@timothybsmith.com>
2013-04-02 12:59:12 -0500

    Fixed the internal linkage problems

--- themes/v6/js/stuff.js
+++ themes/v6/js/stuff.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
   $(".main-content, .content, .video-pusher").fitVids();
 
-  $(".main-content a[href^='http://']").attr("target","_blank");
+  $('a:not([href^="http://timothybsmith.com"]):not([href^="#"]):not([href^="/"]):not([href^="http://dev.ttimsmith:8888/"]) ').attr("target","_blank");
 
   $('.dot-irecommendthis').one('click', function() {
     $(this).addClass('animated bounce');
