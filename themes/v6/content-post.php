087f92e703f78eb629c3f1c319b28a4e49ad7741
Tim Smith <tim@timothybsmith.com>
2013-04-26 15:53:47 -0500

    Updating Jetpack and adding functionality to thank people for helping me out on posts.

--- themes/v6/content-post.php
+++ themes/v6/content-post.php
@@ -4,6 +4,21 @@
 
     <?php the_content(); ?>
 
+    <?php
+                if(get_field('post_thanks')): ?>
+    
+    <hr>
+    <div class="post-helpers">
+      <small><strong class="post-thanks" data-tooltip="The following people helped me with this post.">Thanks to:</strong></small>
+      <small>
+      <?php while(has_sub_field('post_thanks')): ?>
+        <a href="<?php the_sub_field('thanks_person_url'); ?>"><?php the_sub_field('thanks_person_name'); ?></a> 
+      <?php endwhile; ?>
+      </small>
+    </div><!-- end .post-helpers -->
+
+    <?php endif; ?>
+
     <ul class="meta_info">
       <?php previous_post_link('<li class="previous-post">%link</li>', '<span aria-hidden="true" data-icon="r"></span><span class="screen-reader-text">Previous Post</span>'); ?>
       <?php next_post_link('<li class="next-post">%link</li>', '<span aria-hidden="true" data-icon="l"></span><span class="screen-reader-text">Next Post</span>'); ?>
