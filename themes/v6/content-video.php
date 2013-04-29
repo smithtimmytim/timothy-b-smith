20359003b3241359b7d03bd84b62b5d6d9e32230
Tim Smith <tim@timothybsmith.com>
2013-04-29 17:56:09 -0500

    Adding attribution to the homepage too

--- themes/v6/content-video.php
+++ themes/v6/content-video.php
@@ -6,7 +6,7 @@
     
     <h2><a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-    <?php the_content()?> 
-    
+    <?php the_content(); ?>
+
   </article><!-- .entry -->
 </div>
--- themes/v6/content.php
+++ themes/v6/content.php
@@ -3,7 +3,20 @@
     <p class="pubdate"><time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F jS, Y');?></time></p>
     <h2><a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-    <?php the_content()?> 
+    <?php the_content(); ?>
+
+    <?php
+                if(get_field('post_thanks')): ?>
+    
+    <hr>
+    <div class="post-helpers">
+      <small><strong class="post-thanks" data-tooltip="The following people helped me with this post.">Thanks to:</strong></small>
+      <small>
+      <?php while(has_sub_field('post_thanks')): ?>
+        <a href="<?php the_sub_field('thanks_person_url'); ?>"><?php the_sub_field('thanks_person_name'); ?></a> 
+      <?php endwhile; ?>
+      </small>
+    </div><!-- end .post-helpers -->
     
   </article><!-- .entry -->
 </div>
