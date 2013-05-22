4b592eb52203a92ef4b0426e2661fef72a2ad3a7
Tim Smith <tim@timothybsmith.com>
2013-05-22 11:53:54 -0500

    Adding affiliate link

--- themes/v6/footer.php
+++ themes/v6/footer.php
@@ -2,9 +2,9 @@
 <footer>
   <div class="contain">
     <div class="content">
+      <small class="twitter-follow-button"><a href="https://twitter.com/ttimsmith" class="twitter-follow-button" data-show-count="false" data-dnt="true">Follow @ttimsmith</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></small>
       <small>&copy; 2008&ndash;<?php echo date('Y')?> Timothy B. Smith. <a href="http://feeds.feedburner.com/timothybsmith">Subscribe via RSS</a></small>
-      <small class="twitter-follow-button"><a href="https://twitter.com/ttimsmith" class="twitter-follow-button" data-show-count="false" data-dnt="true">Follow @ttimsmith</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-      </small>
+      <small>Kindly hosted by <a href="http://www.mediatemple.net/#a_aid=4f42c6d7d53a5" target="_blank">Media Temple</a>.</small>
     </div>
   </div><!-- .contain -->
 
