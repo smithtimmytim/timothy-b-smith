1117702f2af59b4273e7df19844d9c7460220308
Tim Smith <tim@timothybsmith.com>
2013-05-25 22:49:12 -0500

    working with some jQuery and the input fields

--- themes/v6.5/js/stuff.js
+++ themes/v6.5/js/stuff.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
   $(".main-content, .content, .video-pusher").fitVids();
 
-  $('.main-content a:not([href^="http://timothybsmith.com"]):not([href^="#"]):not([href^="/"]):not([href^="http://dev.ttimsmith:8888/"]) ').attr("target","_blank");
+  $('.main-content a:not([href^="http://ttimsmith.com"]):not([href^="#"]):not([href^="/"]):not([href^="http://dev.ttimsmith:8888/"]) ').attr("target","_blank");
 
   $('.dot-irecommendthis').one('click', function() {
     $(this).addClass('animated bounce');
