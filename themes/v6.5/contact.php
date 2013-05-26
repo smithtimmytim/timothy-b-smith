5855e46b353f1f031c1e766e64d5158877587616
Tim Smith <tim@timothybsmith.com>
2013-05-25 23:25:26 -0500

    Updating the contact page

--- themes/v6.5/contact.php
+++ themes/v6.5/contact.php
@@ -11,7 +11,7 @@ Template Name: Contact
 	<div class="contain">
 		<div class="content">
 		    <h2 class="statement">Need to Get in Touch? You're in the Right Place!</h2>
-		    <p>Thanks for reaching out! I try getting to my email pretty quickly, so you should be hearing from me soon. If you don't like using forms, you can always email me at <a href="mailto:tim@timothybsmith.com?subject=What's Up Homie?">tim@timothybsmith.com</a>.</p>
+		    <p>Thanks for reaching out! I try getting to my email pretty quickly, so you should be hearing from me soon. If you don't like using forms, you can always email me at <strong>smith[at]ttimsmith[dot]com</strong>.</p>
 
 		    <?php gravity_form(1, false, false, false, '', false); ?>
 		 
