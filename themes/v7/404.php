f2a502b28499b11ae6e24f400ac6f54cd24e8ac5
Tim Smith <tim@timothybsmith.com>
2013-06-28 16:44:39 -0500

    Styling 404 page

--- themes/v7/404.php
+++ themes/v7/404.php
@@ -2,12 +2,12 @@
 <?php get_header() ?>
 
 <section role="main" class="main-content">
-  <div class="contain">
-    <div class="content">
-        <h1 class="statement">Awwwww snap! What happened? This page doesn't exist!</h2>
-        <p>Should something be here? <a href="/contact">Get in touch</a>. Hey look! Cat mittens!</p>
-        <img src="<?php bloginfo('template_url');?>/img/cat-mittens.gif" alt="Cat Mittens Gif">
-    </div><!-- end .content -->
-  </div><!-- end .contain -->
+  <h1 class="statement">Doh! 404 Dude…</h2>
+  <div class="content">
+    <div class="contain">
+      <p>Should something be here? <a href="/contact">Get in touch</a>. Hey look! Cat mittens!</p>
+      <img src="<?php bloginfo('template_url');?>/img/cat-mittens.gif" alt="Cat Mittens Gif">
+    </div><!-- end .contain -->
+  </div><!-- end .content -->
 </section><!-- end .main-content -->
 <?php get_footer(); ?>
