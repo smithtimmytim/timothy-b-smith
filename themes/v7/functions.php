d80bc410f6340028e0862eff36e817614a27803b
Tim Smith <tim@timothybsmith.com>
2013-07-05 23:15:33 -0500

    Adding Availability link to WP Admin bar

--- themes/v7/functions.php
+++ themes/v7/functions.php
@@ -1,37 +1,5 @@
 <?php
 
-// add_action('init', 'cptui_register_my_cpt');
-// function cptui_register_my_cpt() {
-// register_post_type('case-studies', array( 
-// 'label' => '__('Case Studies')',
-// 'description' => '',
-// 'public' => 'true',
-// 'show_ui' => 'true',
-// 'show_in_menu' => 'true',
-// 'capability_type' => 'post',
-// 'hierarchical' => 'false',
-// 'rewrite' => array('slug' => 'portfolio', 'with_front' => ''),
-// 'query_var' => 'true',
-// 'exclude_from_search' => 'false',
-// 'supports' => array('title','editor','excerpt','trackbacks','custom-fields','comments','revisions','thumbnail','author','page-attributes' ),
-// 'labels' => 'array (
-//   'name' => 'Case Studies',
-//   'singular_name' => 'Case Study',
-//   'menu_name' => 'Case Studies',
-//   'add_new' => 'Add Case Study',
-//   'add_new_item' => 'Add New Case Study',
-//   'edit' => 'Edit',
-//   'edit_item' => 'Edit Case Study',
-//   'new_item' => 'New Case Study',
-//   'view' => 'View Case Study',
-//   'view_item' => 'View Case Study',
-//   'search_items' => 'Search Case Studies',
-//   'not_found' => 'No Case Studies Found',
-//   'not_found_in_trash' => 'No Case Studies Found in Trash',
-//   'parent' => 'Parent Case Study',
-// )'
-// ) ); }
-
 // Adding Custom Post Type - Case Studies
 
 add_action( 'init', 'create_post_type');
@@ -122,6 +90,30 @@ add_image_size('portfolio-thumb', 640, 9999);
 register_options_page('Hiring');
 
 remove_filter('the_content', 'wptexturize');
+
+
+// Messing with WP Admin Bar
+
+
+function mytheme_admin_bar_render() {
+    global $wp_admin_bar;
+    // we can remove a menu item, like the Comments link, just by knowing the right $id
+    $wp_admin_bar->remove_menu('comments');
+    $wp_admin_bar->remove_menu('wpseo-menu');
+    // or we can remove a submenu, like New Link.
+    $wp_admin_bar->remove_menu('new-link', 'new-content');
+    $wp_admin_bar->remove_menu('new-media', 'new-content');
+    $wp_admin_bar->remove_menu('new-user', 'new-content');
+    // we can add a submenu item too
+    $wp_admin_bar->add_menu( array(
+        'parent' => false,
+        'id' => 'work-availability',
+        'title' => __('Availability'),
+        'href' => admin_url( 'admin.php?page=acf-options-hiring')
+    ) );
+}
+// and we hook our function via
+add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
 
 
 
